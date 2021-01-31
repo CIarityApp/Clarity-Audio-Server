9436bdfd9402128e5031d42b6bea26797531aee9
Andy Wang <andynmnwang@gmail.com>
2021-01-31 06:05:42 -0600

    move files to root and init flask app

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, request
-
+import clean-audio.py
 app = Flask(__name__)
+
+UPLOAD_DESTINATION = 'assets/'
 
 msgs = []
 new_msgs = []
 
+@app.route('/')
+def root():
+    return 'Hello, World'
+
 @app.route("/outgoing")
 def outgoing():
-    # return something
+    return 'Hello, Outgoing'
 
 @app.route("/incoming", methods=['POST'])
 def incoming():
-    # msgs.append(request.get_something)
     return '', 204
-
     
 if __name__ == "__main__":
     app.run(debug=True)
